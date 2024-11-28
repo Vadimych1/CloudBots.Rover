39b959c9cf606cddefe394757fb3500b08294bf9
vadimych1 <vadimych1@ROS-Ubuntu.myguest.virtualbox.org>
2024-11-28 15:32:06 +0300

    Fix & some update

--- main/main/main.py
+++ main/main/main.py
@@ -7,7 +7,7 @@ class AsyncReceiver(Node):
     def __init__(self):
         super().__init__('sensor_data_receiver')
 
-        self.cli = self.create_client(SensorInterface, 'sensor_data_receiver')
+        self.cli = self.create_client(SensorInterface, 'send_service')
         
         while not self.cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info("Waiting for service to connect.")
@@ -15,19 +15,21 @@ class AsyncReceiver(Node):
         self.req = SensorInterface.Request()
 
     def get_sensors_data(self, videoData: bool):
-        self.req.sendVideoData = videoData
+        self.req.send_video_data = videoData
         return self.cli.call_async(self.req)
     
 def main():
+    print("Initializing...")
     rclpy.init()
 
     recv = AsyncReceiver()
     future = recv.get_sensors_data(False)
+    print("Running")
     rclpy.spin_until_future_complete(recv, future)
 
     response = future.result()
 
-    recv.get_logger().info(f"Got message! {response.message_out}")
+    recv.get_logger().info(f"Got message! {response}")
 
     recv.destroy_node()
     rclpy.shutdown()
--- sensors/sensors/main.py
+++ sensors/sensors/main.py
@@ -5,28 +5,58 @@ from rclpy.node import Node
 
 class Service(Node):
     
-    
     def __init__(self):
-        super().__init__('main_service')
+        super().__init__('sensor_service')
         self.send_srv = self.create_service(SensorInterface, 'send_service', self.sensor_data_callback)
         self.receive_srv = self.create_service(ReceiveSensorInterface, 'receive_service', self.receive_data_callback)
     
+        self.videoData = ""
+        self.global_x = 0.0
+        self.global_y = 0.0
+        self.global_z = 0.0
+
     def receive_data_callback(self, request, response):
+        if request.video_data:
+            self.videoData 
+
+        if request.global_x:
+            self.global_x = request.global_x
         
+        if request.global_y:
+            self.global_y = request.global_y
+            
+        if request.global_z:
+            self.global_z = request.global_z
+
+        response.success = True
+        response.message = ""
+
+        return response
 
     def sensor_data_callback(self, request, response):
-        send_video = request.sendVideoData
+        send_video = request.send_video_data
 
         if send_video:
-            ...
+            response.video_data = self.videoData
+        else:
+            response.video_data = ""
+
+        response.global_x = self.global_x
+        response.global_y = self.global_y
+        response.global_z = self.global_z
+
+        response.success = True
+        response.message = ""
 
         return response
 
 def main():
+    print("Initializing...")
     rclpy.init()
 
     service = Service()
 
+    print("Running")
     rclpy.spin(service)
     rclpy.shutdown()
 
